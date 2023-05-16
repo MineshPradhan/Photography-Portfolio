6b38a3d52bc2de49a0cda58427c9ca3a445f7a4f
MineshPradhan <mineshpradhan2002@gmail.com>
2023-05-16 22:46:05 +0530

    nav bar changed

--- main.js
+++ main.js
@@ -17,9 +17,43 @@ textButtons.forEach(textButton => {
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
-    spaceBetween: 30,
+    spaceBetween: 20,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
+    breakpoints:{
+      599:{
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      1023:{
+        slidesPerView: 3,
+        spaceBetween: 60
+      }
+    }
   });
+
+  const nav = document.querySelector('.nav__links');
+  const openNavBtn = document.querySelector('#nav__toggle-open');
+  const closeNavBtn = document.querySelector('#nav__toggle-close');
+
+  const openNav = () => {
+    nav.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline-block';
+  }
+
+  openNavBtn.addEventListener('click', openNav);
+
+  const closeNav = () => {
+    nav.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display = 'none';
+  }
+
+  closeNavBtn.addEventListener('click', closeNav);
+
+  nav.querySelectorAll('li a').forEach(navLink => {
+    navLink.addEventListener('click', closeNav);
+  })
